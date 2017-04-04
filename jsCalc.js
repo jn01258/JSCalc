3f0bd92d881d8665b670844864b32ec9d2591629
Andrea <j.andrea.nelson@gmail.com>
2017-04-03 20:14:36 -0400

    numerical value in console

--- jsCalc.js
+++ jsCalc.js
@@ -13,3 +13,25 @@ function multiplication (a,b){
 function division (a,b){
   return a/b;
 }
+
+// when the user clicks on one of these squares,
+ // it displays that number in the console log.
+ // This can be done by assigning a function to the onclick() property of the div
+
+ var squares = document.getElementsByTagName("div");
+
+ function initiate() {
+     clickNumber();
+
+ }
+
+ function clickNumber() {
+     for (var i = 0; i < squares.length; i++) {
+     	squares[i].addEventListener("click", function() {
+         var toConsole = parseInt(this.getAttribute("number-value"));
+         console.log(toConsole);
+
+       });
+     }
+ }
+initiate();
